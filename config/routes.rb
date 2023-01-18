5184f6e52db5857d112083751ee153c5b747846b
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2023-01-18 22:09:32 +0000

    Add target

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "misc", :action => "homepage" })
 
+  get("/rock", { :controller => "game", :action => "user_plays_rock" })
 
+  get("/paper", { :controller => "game", :action => "user_plays_paper" })
 
+  get("/scissors", { :controller => "game", :action => "user_plays_scissors" })
 end
